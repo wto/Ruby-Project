083724b70b61b621a1999869f7ba8fe4a23eb161
Thomas Oppenheim <thomas@wtoppenheim.net>
2012-11-20 16:12:42 -0500

    Followed a tutorial to create a rails app where you can add status updates.

---
+++ RubyProject/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyProject::Application.config.secret_token = '11fb304d6125916a20421e637bb6961b750a16ac4757bb849da0584d876b1f390d7476d3c0c7eb171649893ace3b52731abc219bb3fc4445aa1784a0e480f565'
